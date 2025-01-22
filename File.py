098adfc3f50316e82bb34be52f7520bcf4eb2db6
RAMZO-TRT <115856285+RAMZO-TRT@users.noreply.github.com>
2025-01-22 22:17:52 +0500

    Update File.py

--- File.py
+++ File.py
@@ -2,4 +2,4 @@ import os,platform
 os.system('git pull')
 Ramzo=platform.architecture()[0]
 if Ramzo=="32bit":print('32 Bit Upload Soon')
-elif Ramzo=="64bit":__import__("trt_enc")
+elif Ramzo=="64bit":__import__("File_Making")
